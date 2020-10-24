9215e7de2ee79735633ddda7af8412dcb58447f6
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2020-10-25 01:36:18 +0300

    Updates

---
+++ precache-manifest.b91034b646aa328211ef9f3bd5203610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059dc92bfc62375e05ae5e3f1f7a24cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16aa20cf00de3dcbc9ac",
+    "url": "/static/css/main.ff178b6c.chunk.css"
+  },
+  {
+    "revision": "7b9af4c0df5f8bfcbe2e",
+    "url": "/static/js/2.405f7510.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.405f7510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16aa20cf00de3dcbc9ac",
+    "url": "/static/js/main.3d341119.chunk.js"
+  },
+  {
+    "revision": "d04fac08b2c8611c842e",
+    "url": "/static/js/runtime-main.7a4e2425.js"
+  },
+  {
+    "revision": "2f123d7fe5bf191fd6a416b083f35813",
+    "url": "/static/media/COVID-19.2f123d7f.png"
+  }
+]);
